a8f9997f8c8d3b1098ea940f84d5d06511d9e821
Wilson <lynnx@gmail.com>
2020-08-02 01:33:12 +0000

    first assignment solution

---
+++ Wilson_assignment1_solution.js
@@ -0,0 +1,8 @@
+var myName = 'Wilson Gbedema';
+var myEmail = 'wilsongbedema@gmail.com';
+var myLanguage = 'JavaScript';
+var myBiostack = 'Genomics';
+var myUsername  = 'Wilson';
+
+console.log('Name: ' + myName, '| Email: ' + myEmail, '| Programming Language: '
++ myLanguage, '| Biostack: ' + myBiostack, '| Slack username: ' + myUsername)
